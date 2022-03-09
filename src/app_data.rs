030bdae2e147dc46661827c95afebc2a5eaf5477
George Atkinson <geomyles@yahoo.co.uk>
2022-03-09 15:13:18 +0000

    Basic demo

--- src/app_data.rs
+++ src/app_data.rs
@@ -1,16 +1,36 @@
 use vizia::*;
 
+use crate::alert;
+
 #[derive(Debug, Lens)]
 pub struct AppData {
-
+    pub visible: bool,
+    pub text_size: f32,
 }
 
 pub enum AppEvent {
-
+    Debug(String),
+    Test,
+    SetTextSize(f32),
 }
 
 impl Model for AppData {
     fn event(&mut self, cx: &mut Context, event: &mut Event) {
+        if let Some(app_event) = event.message.downcast() {
+            match app_event {
+                AppEvent::Debug(val) => {
+                    alert(val);
+                    println!("Done");
+                },
 
+                AppEvent::Test => {
+                    self.visible = true;
+                }
+
+                AppEvent::SetTextSize(val) => {
+                    self.text_size = *val;
+                }
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,46 +2,70 @@ use futures::executor;
 use rfd::AsyncFileDialog;
 use vizia::*;
 
+mod app_data;
+use app_data::*;
+
 use wasm_bindgen::prelude::*;
 
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
 fn main() {
     
     let window_description = WindowDescription::new();
 
     let app = Application::new(window_description, |cx|{
-        Button::new(cx, |cx|{
-            //let file = FileDialog::new().pick_file().expect("Failed to select file path");
-            // if let Some(file) = FileDialog::new().pick_file() {
-            //     //cx.emit(AppEvent::AddFilePaths(files.clone()));
-            //     println!("Open File: {:?}", file);
-            // }
 
-            cx.spawn(|cx| {
-                let task = AsyncFileDialog::new()
-                    .pick_file();
-                execute(async {
-                    let file = task.await;
-            
-                    if let Some(file) = file {
-                        // If you are on native platform you can just get the path
-                        #[cfg(not(target_arch = "wasm32"))]
-                        println!("{:?}", file.path());
-            
-                        // If you care about wasm support you just read() the file
-                        file.read().await;
-
-                        //console_log!("Done");
-                    }
-                });
-            });
-                //let data = file.unwrap().read().await;
+        AppData {
+            visible: false,
+            text_size: 0.5,
+        }.build(cx);
         
-        }, |cx|{
-            Label::new(cx, "Open")
-                .width(Pixels(50.0))
-                .font("icon")
-                .child_space(Stretch(1.0))
-        });
+        Button::new(cx, |cx| cx.emit(AppEvent::Test), |cx|{
+            Label::new(cx, "Press Me")
+        }).space(Pixels(10.0));
+
+        VStack::new(cx, |cx|{
+            Slider::new(cx, AppData::text_size, Orientation::Horizontal)
+                .on_changing(|cx, val| cx.emit(AppEvent::SetTextSize(val)))
+                .width(Pixels(200.0))
+                .top(Pixels(50.0))
+                .bottom(Pixels(50.0));
+            Label::new(cx, "This is a web app!").font_size(AppData::text_size.map(|s| 16.0 + s * 40.0));
+        })
+        .space(Pixels(10.0))
+        .visibility(AppData::visible);
+
+        // Button::new(cx, |cx|{
+
+        //     let event_proxy = cx.event_proxy.as_ref().map(|p| p.make_clone()).unwrap();
+        //     let task = AsyncFileDialog::new()
+        //         .pick_file();
+        //     let event = Event::new(AppEvent::Debug("Very Done".to_string())).target(cx.current);
+        //     execute(async move {
+        //         let file = task.await;
+        
+        //         if let Some(file) = file {
+        //             // If you are on native platform you can just get the path
+        //             #[cfg(not(target_arch = "wasm32"))]
+        //             println!("{:?}", file.path());
+        
+        //             // If you care about wasm support you just read() the file
+        //             file.read().await;
+
+        //             event_proxy.send(event).expect("Failed to send message");
+
+        //         }
+        //     });
+        
+        // }, |cx|{
+        //     Label::new(cx, "Open")
+        //         .width(Pixels(50.0))
+        //         .font("icon")
+        //         .child_space(Stretch(1.0))
+        // });
     });
 
     app.run();
