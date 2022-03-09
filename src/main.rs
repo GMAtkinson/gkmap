14741d508adb6ea23993ee7ad8e2f67b731281e1
George Atkinson <geomyles@yahoo.co.uk>
2022-03-09 12:20:24 +0000

    Add canvas and rebuild

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use vizia::*;
 
 
 fn main() {
-    let window_description = WindowDescription::new();
+    let window_description = WindowDescription::new().with_canvas("win");
     let app = Application::new(window_description, |cx|{
         Label::new(cx, "test");
     });
